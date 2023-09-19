56f3ff4fdf45098fb3ce3df8dfdcb7a95f4a61f3
RudsonAbraao <rudson.a.s.silva@gmail.com>
2023-09-19 12:15:47 -0300

    portifolio finalizado

--- src/Componentes/Apresentacao/Apresentacao.jsx
+++ src/Componentes/Apresentacao/Apresentacao.jsx
@@ -1,13 +1,13 @@
 const Apresentacao = () => {
     return (
         <div className="flex flex-col">
-            <div className="flex md:justify-between mt-10 md:mt-20 md:items-center">
+            <div className="flex md:justify-center gap-60 mt-10 md:mt-20 md:items-center">
                 <div className="text-corTexto pt-2 flex flex-col items-center  w-full md:w-auto text-center md:text-left gap-2">
                     <h1 className="text-[24px] font-bold">Olá, sou  Rudson Abraão</h1>
                     <h4 className="text-cinza">Desenvolvedor Front-End</h4>
                     <a href="https://drive.google.com/file/d/1hrO-QqXBZGGyb385YczgsaIZtSBrlJLv/view" target="_blank" className="bg-verde py-1 px-2 rounded-sm text-fundo md:py-2 md:px-4 font-medium text-md md:text-xl hover:animate-pulse" >Download CV</a>
                 </div>
-                <img src="/imagens/ilustracao.svg" alt="ilustração de um progamador" className="hidden md:block w-[300px]" />
+                <img src="/imagens/ilustracao.svg" alt="ilustração de um progamador" className="hidden lg:block w-[300px]" />
             </div>
             <div className=" self-center max-w-lg ">
                 <a name="sobremim" className="link">Sobre mim</a>
--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import Menu from "../Menu/Menu";
 
 const Header = ()=> {
     return(
-        <div className=" text-corTexto flex justify-between">
+        <div className=" text-corTexto flex justify-between lg:px-10">
             <h1 className="font-bold md:text-3xl">Portifólio</h1>
             <Menu/>
         </div>
